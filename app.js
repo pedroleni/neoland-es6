1bf7f1c7e9a877c76f39edc1e846d910c4d3cde2
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-22 13:10:43 +0200

    Iteration#1

---
+++ app.js
@@ -0,0 +1,29 @@
+//---------------------------------------------------------------------------------------------------------
+//---------------------------------**Iteración #1: Arrows**-------------------------------------------------
+//---------------------------------------------------------------------------------------------------------
+/*Crea una arrow function que tenga dos parametros a y b y
+que por defecto el valor de a = 10 y de b = 5. Haz que la función muestre
+por consola la suma de los dos parametros.*/
+
+const suma= (a = 10 , b =5)=>{
+    let sum= a+b;
+    return sum;
+}
+
+//1.1 Ejecuta esta función sin pasar ningún parametro
+
+console.log(suma())
+
+//1.2 Ejecuta esta función pasando un solo parametro
+
+console.log(suma(11))
+
+
+//1.3 Ejecuta esta función pasando dos parametros
+
+console.log(suma(12,3))
+
+
+
+
+
