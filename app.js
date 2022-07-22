f68295bea1a9f73ab9d7704f67e34f84bb32901c
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-22 13:44:32 +0200

    Iteration#3

--- app.js
+++ app.js
@@ -90,3 +90,64 @@ console.log(carFunction().itv)
 const [var1 , var2, var3]= carFunction().itv
 console.log(var1)
 
+//---------------------------------------------------------------------------------------------------------
+//------------------------------------**Iteración #3: Spread Operator**-----------------------------------
+//---------------------------------------------------------------------------------------------------------
+
+/*3.1 Dado el siguiente array, crea una copia usando spread operators.*/
+const pointsList2 = [32, 54, 21, 64, 75, 43]
+
+const copia = [...pointsList2];
+console.log(copia)
+
+/*3.2 Dado el siguiente objeto, crea una copia usando spread operators.*/
+const toy2 = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const copia2 = {...toy2};
+console.log(copia2)
+
+/*3.3 Dado los siguientes arrays, crea un nuevo array juntandolos usando
+spread operatos.*/
+const pointsList = [32, 54, 21, 64, 75, 43];
+const pointsLis2 = [54,87,99,65,32];
+
+const copia3 = [...pointsList, ...pointsLis2]
+console.log(copia3)
+
+/*3.4 Dado los siguientes objetos. Crea un nuevo objeto fusionando los dos
+con spread operators.*/
+const toy = {name: 'Bus laiyiar', date: '20-30-1995', color: 'multicolor'};
+const toyUpdate = {lights: 'rgb', power: ['Volar like a dragon', 'MoonWalk']}
+
+const copia4 ={
+    ...toy,
+    ...toyUpdate
+}
+
+console.log(copia4)
+
+
+/*3.5 Dado el siguiente array. Crear una copia de él eliminando la posición 2
+pero sin editar el array inicial. De nuevo, usando spread operatos.*/
+const colors = ['rojo', 'azul', 'amarillo', 'verde', 'naranja'];
+
+function eliminar(array, indice) {
+    var resultado = []
+    for (var i = 0; i < array.length; i++) {
+      if (i !== indice) {
+        resultado.push(array[i]);
+      }
+    }
+    return resultado;
+  }
+
+const copia5 = [...colors];
+let finalcopia= eliminar (copia5, 2)
+console.log(finalcopia)
+
+
+
+
+
+
+
+
