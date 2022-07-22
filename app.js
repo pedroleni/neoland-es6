71fa70c40d7c4db207a73081747a5289cf12b03c
Pedro Lérida Nieto <pedroleridanieto@hotmail.com>
2022-07-22 13:11:43 +0200

    Iteration#2

--- app.js
+++ app.js
@@ -25,5 +25,68 @@ console.log(suma(12,3))
 
 
 
+//---------------------------------------------------------------------------------------------------------
+//---------------------------------------**Iteración #2: Destructuring**-----------------------------------
+//---------------------------------------------------------------------------------------------------------
 
+/*2.1 En base al siguiente javascript, crea variables en base a las propiedades
+del objeto usando object destructuring e imprimelas por consola. Cuidado,
+no hace falta hacer destructuring del array, solo del objeto.*/
+
+const game = {title: 'The last us 2', gender: ['action', 'zombie', 'survival'], year: 2020}
+
+
+const {title, gender, year} = game
+console.log(title)
+console.log(gender) 
+console.log(year)
+
+/*2.2 En base al siguiente javascript, usa destructuring para crear 3 variables
+llamadas fruit1, fruit2 y fruit3, con los valores del array. Posteriormente
+imprimelo por consola.*/
+
+
+const fruits = ['Banana', 'Strawberry', 'Orange'];
+
+
+const [fruit1 , fruit2, fruit3] = fruits;
+
+
+console.log(fruit1)
+console.log(fruit2)
+console.log(fruit3)
+
+
+/*2.3 En base al siguiente javascript, usa destructuring para crear 2
+variables igualandolo a la función e imprimiendolo por consola.*/
+
+const animalFunction = () => {
+    return {name: 'Bengal Tiger', race: 'Tiger'}
+};
+
+const variableUno= animalFunction().name
+
+const variableDos= animalFunction().race
+
+console.log(variableUno)
+console.log(variableDos)
+
+
+/*2.4 En base al siguiente javascript, usa destructuring para crear las
+variables name y itv con sus respectivos valores. Posteriormente crea
+3 variables usando igualmente el destructuring para cada uno de los años
+y comprueba que todo esta bien imprimiendolo.*/
+
+const car = {name: 'Mazda 6', itv: [2015, 2011, 2020] }
+
+const carFunction = () => {
+    return {name: 'Mazda 6', itv: [2015, 2011, 2020]}
+};
+
+console.log(carFunction().name)
+console.log(carFunction().itv)
+
+
+const [var1 , var2, var3]= carFunction().itv
+console.log(var1)
 
